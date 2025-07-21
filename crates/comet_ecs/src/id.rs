88fda5c654ad2db66fa4c51e2762e7574591fe01
lisk77 <lisk77@protonmail.com>
2025-07-21 03:01:40 +0200

    fix(ecs): added safety boundaries to the IdQueue

--- crates/comet_ecs/src/id.rs
+++ crates/comet_ecs/src/id.rs
@@ -1,40 +1,45 @@
 #[derive(Debug, Clone)]
 pub struct IdQueue {
-	queue: Vec<u32>
+    queue: Vec<u32>,
 }
 
 impl IdQueue {
-	pub fn new() -> Self {
-		Self { queue: Vec::new() }
-	}
+    pub fn new() -> Self {
+        Self { queue: Vec::new() }
+    }
 
-	pub fn from_vec(queue: Vec<u32>) -> Self {
-		Self { queue }
-	}
+    pub fn from_vec(queue: Vec<u32>) -> Self {
+        Self { queue }
+    }
 
-	pub fn front(&self) -> Option<u32> {
-		Some(self.queue[0])
-	}
+    pub fn front(&self) -> Option<u32> {
+        if self.queue.len() > 0 {
+            return Some(self.queue[0]);
+        }
+        None
+    }
 
-	pub fn enqueue(&mut self, id: u32) {
-		self.queue.push(id)
-	}
+    pub fn enqueue(&mut self, id: u32) {
+        self.queue.push(id)
+    }
 
-	pub fn sorted_enqueue(&mut self, id: u32) {
-		self.enqueue(id);
-		self.queue.sort();
-	}
+    pub fn sorted_enqueue(&mut self, id: u32) {
+        self.enqueue(id);
+        self.queue.sort();
+    }
 
-	pub fn dequeue(&mut self) -> Option<u32> {
-		Some(self.queue.remove(0))
+    pub fn dequeue(&mut self) -> Option<u32> {
+        if self.queue.len() > 0 {
+            return Some(self.queue.remove(0));
+        }
+        None
+    }
 
-	}
+    pub fn is_empty(&self) -> bool {
+        self.queue.len() == 0
+    }
 
-	pub fn is_empty(&self) -> bool {
-		self.queue.len() == 0
-	}
-
-	pub fn size(&self) -> u32 {
-		self.queue.len() as u32
-	}
+    pub fn size(&self) -> u32 {
+        self.queue.len() as u32
+    }
 }
