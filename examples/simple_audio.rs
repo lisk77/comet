90f411f974a68c9a0b9cd1ba952913de4cc0b8bf
lisk77 <lisk77@protonmail.com>
2026-03-28 20:10:35 +0100

    refactor(examples): `simple_audio` now uses the headless run

--- examples/simple_audio.rs
+++ examples/simple_audio.rs
@@ -1,16 +1,16 @@
 use comet::prelude::*;
 
-fn setup(app: &mut App, _renderer: &mut RenderHandle2D) {
+fn setup(app: &mut App) {
     // Load the audio clip (name for playback is the file stem: "hit")
     app.load::<AudioClip>("res://sounds/hit.ogg");
     app.play_audio("hit", true);
 }
 
-fn update(_app: &mut App, _renderer: &mut RenderHandle2D, _dt: f32) {}
+fn update(_app: &mut App, _dt: f32) {}
 
 fn main() {
     App::new()
         .with_title("Comet Sound Example")
         .with_module(AudioModule::new())
-        .run::<Renderer2D>(setup, update);
+        .run_headless(setup, update);
 }
